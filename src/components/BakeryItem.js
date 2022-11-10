9e04e359cfa2c2cb862b22875aef97819675fb25
jimmyjianchen <jimmy_chen@brown.edu>
2022-11-09 21:33:46 -0500

    finished lab

--- src/components/BakeryItem.js
+++ src/components/BakeryItem.js
@@ -1,1 +1,12 @@
 // TODO: create a component that displays a single bakery item
+export default function BakeryItem(props) {
+    return (
+        <div>
+            <h2>{props.item.name}</h2>
+            <p>{props.item.description}</p>
+            <p>${props.item.price}</p>
+            <img src={props.item.image}/>
+            <button onClick={()=>props.updateCart(props.item)}>Add to Cart</button>
+        </div>
+    )
+}
